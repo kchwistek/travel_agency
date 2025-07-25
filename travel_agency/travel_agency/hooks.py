1308fed9076380bf26dfb1fc00f6dbc56c31020f
Karel Chwistek <k.chwistek@volny.cz>
2025-07-25 00:02:26 +0000

    Remove unused unicode_literals import from travel sales related Python files.

---
+++ travel_agency/travel_agency/hooks.py
@@ -0,0 +1,111 @@
+# Copyright (c) 2024, Element Labs and contributors
+# For license information, please see license.txt
+
+app_name = "travel_agency"
+app_title = "Travel Agency"
+app_publisher = "Element Labs"
+app_description = "ERPNext Module for Travel Agents"
+app_icon = "octicon octicon-briefcase"
+app_color = "blue"
+app_email = "saeed@elementlabs.xyz"
+app_license = "MIT"
+
+# Includes in <head>
+# ------------------
+# app_include_css = "/assets/travel_agency/css/travel_agency.css"
+# app_include_js = "/assets/travel_agency/js/travel_agency.js"
+
+# Home Pages
+# ----------
+# home_page = "login"
+# role_home_page = {
+#     "Role": "home_page"
+# }
+
+# Generators
+# ----------
+# website_generators = ["Web Page"]
+
+# Installation
+# ------------
+# before_install = "travel_agency.install.before_install"
+# after_install = "travel_agency.install.after_install"
+
+# Desk Notifications
+# ------------------
+# notification_config = "travel_agency.notifications.get_notification_config"
+
+# Permissions
+# -----------
+# permission_query_conditions = {
+#     "DocType": "travel_agency.doctype.doctype.permission_query_conditions"
+# }
+# has_permission = {
+#     "DocType": "travel_agency.doctype.doctype.has_permission"
+# }
+
+# Document Events
+# ---------------
+# doc_events = {
+#     "*": {
+#         "on_update": "method",
+#         "on_cancel": "method",
+#         "on_trash": "method"
+#     }
+# }
+
+# Scheduled Tasks
+# ---------------
+# scheduler_events = {
+#     "all": [
+#         "travel_agency.tasks.all"
+#     ],
+#     "daily": [
+#         "travel_agency.tasks.daily"
+#     ],
+#     "hourly": [
+#         "travel_agency.tasks.hourly"
+#     ],
+#     "weekly": [
+#         "travel_agency.tasks.weekly"
+#     ],
+#     "monthly": [
+#         "travel_agency.tasks.monthly"
+#     ],
+# }
+
+# Testing
+# -------
+# before_tests = "travel_agency.install.before_tests"
+
+# Overriding Methods
+# ------------------
+# override_whitelisted_methods = {
+#     "frappe.desk.doctype.event.event.get_events": "travel_agency.event.get_events"
+# }
+
+# User Data Protection
+# --------------------
+# user_data_fields = [
+#     {
+#         "doctype": "DocType",
+#         "filter_by": "filter_by",
+#         "redact_fields": ["field1", "field2"],
+#         "partial": 1,
+#     },
+#     {
+#         "doctype": "DocType",
+#         "filter_by": "filter_by",
+#         "partial": 1,
+#     },
+#     {
+#         "doctype": "DocType",
+#         "strict": False,
+#     },
+# ]
+
+# Authentication and authorization
+# --------------------------------
+# auth_hooks = [
+#     "travel_agency.auth.validate"
+# ] 
